e075952a9f2cdf8e921ff93a14fbf1444790d493
Johan Ospina Nunez <Aospina@intergrupo.com>
2019-11-13 01:08:43 -0500

    version 1

---
+++ WebApplication1/EmployeeBL/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeBL.Abstracts;
+using Newtonsoft.Json.Linq;
+using WebApplication3.Models;
+
+namespace EmployeeBL
+{
+    public class Employee : IEmployee
+    {
+
+        public IList<Employees> CreateProjectModified()
+        {
+            string baseUrl = "http://masglobaltestapi.azurewebsites.net/api/Employees";
+            var json = new WebClient().DownloadString(baseUrl);
+
+
+            JArray PersonArray = JArray.Parse(json);
+
+            IList<Employees> persona = PersonArray.Select(p => new Employees
+            {
+                name = (string)p["name"],
+                contractTypeName = (string)p["contractTypeName"],
+                roleId = (string)p["roleId"],
+                roleName = (string)p["roleName"],
+                roleDescription = (string)p["roleDescription"],
+                hourlySalary = (int)p["hourlySalary"],
+                monthlySalary = (int)p["monthlySalary"]
+
+            }).ToList();
+
+
+            foreach (var item in persona)
+            {
+                if (item.contractTypeName == "HourlySalaryEmployee")
+                {
+                    item.Total = 120 * item.hourlySalary * 12;
+                }
+                else
+                {
+                    item.Total = item.monthlySalary * 12;
+                }
+
+            }
+
+            return persona;
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Autofac;
+using EmployeeBL;
+using EmployeeBL.Abstracts;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employees
+        public ActionResult Index()
+        {
+            var app = new ContainerBuilder();
+            app.RegisterType<Employee>().As<IEmployee>();
+            var builder = app.Build();
+            var _employee = builder.Resolve<IEmployee>();
+            var response = _employee.CreateProjectModified();
+            return View(response);
+        }
+    }
+}
